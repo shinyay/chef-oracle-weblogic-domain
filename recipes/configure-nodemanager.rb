60ae06b6bcf0a5e26473c220046456b182b327f5
Shinya Yanagihara <shinya.com@gmail.com>
2016-11-09 06:23:29 +0000

    Add a recipe for configuring password of nodemanager

---
+++ recipes/configure-nodemanager.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: oracle-weblogic-domain
+# Recipe:: create
+#
+# Copyright (c) 2016 Shinya Yanagihara, All Rights Reserved.
+
+# log  "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Starting execution phase"
+puts "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Starting compile phase"
+
+#############################
+# Create WLST Script file for creating domain
+template "#{node['weblogic-domain']['response_file_dir']}/configure_#{node['weblogic-domain']['domain_name']}_nodemanager.py" do
+  source "configure_nodemanager.py.erb"
+  owner node['weblogic-domain']['owner']
+  group node['weblogic-domain']['group']
+  mode '0755'
+end
+
+#############################
+# Run wlst and Create a WLS Domain
+execute "wlst.sh configure_#{node['weblogic-domain']['domain_name']}_nodemanager.py" do
+  environment "CONFIG_JVM_ARGS" => "-Djava.security.egd=file:/dev/./urandom"
+  command <<-EOH 
+    . #{node['weblogic-domain']['wls_home']}/server/bin/setWLSEnv.sh
+    #{node['weblogic-domain']['oracle_common']}/common/bin/wlst.sh #{node['weblogic-domain']['response_file_dir']}/configure_#{node['weblogic-domain']['domain_name']}_nodemanager.py
+  EOH
+  user node['weblogic-domain']['user']
+  group node['weblogic-domain']['group']
+  action :run
+  creates "#{node['weblogic-domain']['domain_home']}/#{node['weblogic-domain']['domain_name']}/servers"
+end
+
+# log  "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Finished execution phase"
+puts "####{cookbook_name}::#{recipe_name} #{Time.now.inspect}: Finished compile phase"
